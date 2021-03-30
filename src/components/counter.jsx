9153e190fa79c3bbe74bb2eb3ab5437393a58e51
emzubair <muhammad.zubair@arbisoft.com>
2021-03-30 22:16:58 +0500

    conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,35 +2,19 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count : 0,
-        imageURL: "https://picsum.photos/200",
         tags : ['tag1', 'tag2', 'tag3']
     }
-    styles = {
-        fontSize: 20,
-        fontWeight: "bold"
+
+    renderTags() {
+        if (this.state.tags.length === 0) { return <h1>No tags found</h1>}
+        return <ul>{this.state.tags.map( tag => <li key={tag}>{tag}</li>)}</ul>
     }
     render() { 
         return (<React.Fragment>
-            <img src={this.state.imageURL} alt="Random Image"></img>
-            <h1 style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</h1>
-            <button style={{fontSize:20}} className="btn btn-secondary btn-sm">Icrement</button>
-            <ul>
-                {this.state.tags.map( tag => <li key={tag}>{tag}</li>)}
-            </ul>
+                { this.state.tags.length === 0 &&  "Welcome to JS Truthy Falsy Expressions"}
+                {this.renderTags()}
             </React.Fragment>);
 
-    }
-
-    getBadgeClasses() {
-        let counterClasses = "badge m-2 badge-";
-        counterClasses += this.state.count === 0 ? "warning" : "primary";
-        return counterClasses;
-    }
-
-    formatCount() {
-        const {count} = this.state;
-        return count === 0 ? "Zero" : count;
     }
 }
  
