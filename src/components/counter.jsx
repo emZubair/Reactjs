4340c550f5ae85ea384218681112767f320c2722
emzubair <muhammad.zubair@arbisoft.com>
2021-04-01 23:51:33 +0500

    Passed event by props & communicated to parent object when clicked

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        value : this.props.value,
+        value : this.props.counter.value,
         imageURL: "https://picsum.photos/200",
         tags : ['tag1', 'tag2', 'tag3']
     }
@@ -21,12 +21,16 @@ class Counter extends Component {
         this.setState({value: this.state.value+1})
         console.log(`Icremented value:${this.state.value}`)
     }
+    handleDelete = () => {
+        console.log("Delete button clicked")
+    }
     render() { 
         return (<React.Fragment>
             {this.props.children}
             <img src={this.state.imageURL} alt="Random Image"></img>
             <h1 style={this.styles} className={this.getBadgeClasses()}>{this.formatValue()}</h1>
             <button style={{fontSize:20}} onClick={() => this.handleIncrement("Zubair", 33)} className="btn btn-secondary btn-sm">Icrement</button>
+            <button className="btn btn-danger btn-sm m-2" onClick={()=> this.props.onDelete(this.props.counter.id)}>Delete</button>
             <ul>
                 {this.state.tags.map( tag => <li key={tag}>{tag}</li>)}
             </ul>
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,9 +10,18 @@ class Counters extends Component {
         {id: 5, value: 1},
         {id: 6, value: 2}
     ] }
+
+    handleDelete = (counterID) => {
+        console.log("handleDelete "+ counterID)
+        const counters = this.state.counters.filter(counter => counter.id !== counterID)
+        this.setState({counters})
+    }
+
     render() { 
-        return ( <div>{this.state.counters.map(counter => 
-            <Counter key={counter.id} value={counter.value} selected={true}>
+        return ( <div>
+            <button className="btn btn-primary btn-sm m-2">Reset State</button>
+            {this.state.counters.map(counter => 
+            <Counter key={counter.id} counter={counter} onDelete={this.handleDelete} selected={true}>
                 <h1>Booth number:{counter.id}</h1>
             </Counter>
         )}
